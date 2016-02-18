6611b5c07009d0f09b673afbfb78ae8e77263962
Francesco Abbate <francesco.bbt@gmail.com>
2016-02-18 14:57:22 +0100

    Tutorial step, add contact form

--- my-app.js
+++ my-app.js
@@ -4,6 +4,8 @@ var contacts = [
     {key: 3, name: "Joe"},
 ]
 
+var newContact = {name: "", email: "", description: ""}
+
 var ContactItem = React.createClass({
 	propTypes: {
 		name: React.PropTypes.string.isRequired,
@@ -20,6 +22,22 @@ var ContactItem = React.createClass({
 	}
 });
 
+var ContactForm = React.createClass({
+	propTypes: {
+		contact: React.PropTypes.object.isRequired,
+	},
+
+	render: function() {
+		var contact = this.props.contact;
+		return React.createElement('form', {},
+			React.createElement('input', {type: 'text', placeholder: 'Name (required)', value: contact.name}),
+			React.createElement('input', {type: 'email', placeholder: 'Email', value: contact.email}),
+			React.createElement('textarea', {placeholder: 'Description', value: contact.description}),
+			React.createElement('button', {type: 'submit'}, 'Add Contact')
+		);
+	}
+});
+
 var contactItemsElements = contacts
 	.filter(function(contact) { return contact.email; })
 	.map(function(contact) { return React.createElement(ContactItem, contact); });
@@ -27,7 +45,8 @@ var contactItemsElements = contacts
 var rootElement =
 	React.createElement('div', {},
 		React.createElement('h1', {}, "Contacts"),
-		React.createElement('ul', {}, contactItemsElements)
+		React.createElement('ul', {}, contactItemsElements),
+		React.createElement(ContactForm, {contact: newContact})
 	);
 
 ReactDOM.render(rootElement, document.getElementById('react-app'))
